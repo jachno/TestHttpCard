464eb99e860523407f3f489416703c1cdeb69040
Jamie Norman <>
2018-09-20 17:16:22 +0100

    init add

---
+++ resources/js/card.js
@@ -0,0 +1,3 @@
+Nova.booting((Vue, router) => {
+    Vue.component('test-http-card', require('./components/Card'));
+})
---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Zttp\Zttp;
+
+/*
+|--------------------------------------------------------------------------
+| Card API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you may register API routes for your card. These routes
+| are loaded by the ServiceProvider of your card. You're free to add
+| as many additional routes to this file as your card may require.
+|
+*/
+
+ Route::get('/endpoint', function (Request $request) {
+     $response = Zttp::get('https://novapackages.com/api/recent');
+        return $response->json()['data'];
+ });
